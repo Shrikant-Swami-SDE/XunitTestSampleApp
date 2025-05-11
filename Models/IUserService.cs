a8c72bed46578210cdbcf05e89dce6f2854f00ba
HP <HP@DESKTOP-F73KVCN>
2025-05-11 12:04:06 +0530

    Add project files.

---
+++ Models/IUserService.cs
@@ -0,0 +1,11 @@
+﻿namespace XunitTestSampleApp.Models
+{
+    public interface IUserService
+    {
+        User GetUserById(int userId);
+        IEnumerable<User> GetAllUsers();
+        void AddUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int userId);
+    }
+}
